140f995e4ba12470c74469833d49f4f7b68e38c3
Julhan Abdul Malik <julhanabdulmalik@gmail.com>
2025-07-20 09:43:35 +0700

    update Rendering List

--- src/components/Pages/products.jsx
+++ src/components/Pages/products.jsx
@@ -1,28 +1,47 @@
 import CardProduct from "../Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    name: "Nike Air Max 90",
+    imagesName: "shoes-1.jpg",
+    price: 100,
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
+          illo eaque, laborum quibusdam soluta nostrum sed iure est a quas
+          voluptate obcaecati mollitia fugit earum deserunt cupiditate non
+          itaque maiores?`,
+  },
+  {
+    id: 2,
+    name: "Nike Air Max 270",
+    imagesName: "shoes-1.jpg",
+    price: 200,
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
+          illo eaque.`,
+  },
+  {
+    id: 3,
+    name: "Nike Air Max 720",
+    imagesName: "shoes-1.jpg",
+    price: 80,
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
+          illo eaque, laborum quibusdam soluta nostrum sed iure est a quas
+          voluptate obcaecati mollitia.`,
+  },
+];
+
 const ProductPage = () => {
   return (
     <div className="flex justify-center py-10">
-      <CardProduct>
-        <CardProduct.Header imagesName="shoes-1.jpg" />
-        <CardProduct.Body title="Nike Air Max 90">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
-          illo eaque, laborum quibusdam soluta nostrum sed iure est a quas
-          voluptate obcaecati mollitia fugit earum deserunt cupiditate non
-          itaque maiores?
-        </CardProduct.Body>
-        <CardProduct.Footer price="200" />
-      </CardProduct>
-
-      <CardProduct>
-        <CardProduct.Header imagesName="shoes-1.jpg" />
-        <CardProduct.Body title="Nike Air Max 92">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
-          illo eaque, laborum quibusdam soluta nostrum sed iure est a quas
-          voluptate.
-        </CardProduct.Body>
-        <CardProduct.Footer price="500" />
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct>
+          <CardProduct.Header imagesName={product.imagesName} />
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
